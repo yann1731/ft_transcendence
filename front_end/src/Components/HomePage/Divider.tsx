11ecee774b087f0ca71d8eeec478e18347f457c0
Marc-Andre Lord <mandrelord@gmail.com>
2023-05-11 08:50:15 -0400

    Updated

--- front_end/src/Components/HomePage/Divider.tsx
+++ front_end/src/Components/HomePage/Divider.tsx
@@ -4,3 +4,19 @@ import Stack from '@mui/material/Stack';
 import ChatContainer from './Chat';
 import GameContainer from './Game';
 import ScoresContainer from './Scores';
+
+export default function DividerStack() {
+    return (
+      <div>
+        <Stack
+          direction="row"
+          divider={<Divider orientation="vertical" flexItem />}
+          spacing={3}
+          >
+              <ScoresContainer></ScoresContainer>
+              <GameContainer></GameContainer>
+              <ChatContainer></ChatContainer>
+        </Stack>
+      </div>
+    );
+  }
