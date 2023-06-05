d3a4e759d78c342615c0598393b3c36d59f6b46d
fenix144 <yan_1731@hotmail.com>
2023-06-05 11:22:07 -0400

    Add @nestj/platform-socket.io

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
+import { ChatroomModule } from './chatroom/chatroom.module';
 
 @Module({
-  imports: [PrismaModule, ChatModule],
+  imports: [PrismaModule, ChatModule, UserModule, ChatroomModule],
   controllers: [AppController],
   providers: [AppService],
 })
