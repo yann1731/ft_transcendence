16274ac303c5b0c3b49bc4012fc114ec1c173905
fenix144 <yan_1731@hotmail.com>
2023-06-05 13:35:40 -0400

    Fix script to wait for db

--- packages/backend/script/dbstartup.sh
+++ packages/backend/script/dbstartup.sh
@@ -1,6 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h database -p 5432 -U puser -d transcendance -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
 
 npx prisma migrate dev --name "init"
 
-#to be removed later
+>&2 echo "Postgres is up - executing command"
+
 npm run start:dev
+
+
+#!/bin/sh
+
+# npx prisma migrate dev --name "init"
+
+#to be removed later
+# npm run start:dev
+
+# /bin/sh
