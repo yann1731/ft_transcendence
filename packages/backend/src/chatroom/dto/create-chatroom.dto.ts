61849c91c4fe0f253588ab9bba87a26d84328688
Anton <fenix144@pop-os.localdomain>
2023-06-07 11:48:20 -0400

    define create-chatroom dto

--- packages/backend/src/chatroom/dto/create-chatroom.dto.ts
+++ packages/backend/src/chatroom/dto/create-chatroom.dto.ts
@@ -1,1 +1,7 @@
-export class CreateChatroomDto {}
+import { User, chatRoomState } from "@prisma/client";
+
+export class CreateChatroomDto {
+    state: chatRoomState;
+    userId: string;
+    password?: string;
+}
