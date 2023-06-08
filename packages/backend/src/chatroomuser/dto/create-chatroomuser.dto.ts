53fb68f76dd4aadfcbfdd571cf4fe9556c93837b
yann <yan_1731@hotmail.com>
2023-06-07 20:02:33 -0400

    Add chatroomuser dto

--- packages/backend/src/chatroomuser/dto/create-chatroomuser.dto.ts
+++ packages/backend/src/chatroomuser/dto/create-chatroomuser.dto.ts
@@ -1,1 +1,10 @@
-export class CreateChatroomuserDto {}
+import { userPermission } from "@prisma/client";
+
+export class CreateChatroomuserDto {
+    userId: string;
+    chatroomId: string;
+    permission: userPermission;
+    banStatus: boolean;
+    banUntil: Date | null;
+    muteStatus: boolean;
+}
