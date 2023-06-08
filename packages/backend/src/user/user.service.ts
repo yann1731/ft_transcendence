8f5e12169c3e303bc0854c499f4ae25136387f4b
fenix144 <yan_1731@hotmail.com>
2023-06-08 13:44:53 -0400

    defining service and controller

--- packages/backend/src/user/user.service.ts
+++ packages/backend/src/user/user.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) { }
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const user = new CreateUserDto;
+
+    user.email = createUserDto.email;
+    user.password = createUserDto.password;
+    user.username = createUserDto.username;
+
+    return user;
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.user.findMany;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.prisma.user.findUnique({where: { id }} );
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return ;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return ;
   }
 }
