96bbf9a73bede753f9c6c2431d48ba1c1f35f1c1
fenix144 <yan_1731@hotmail.com>
2023-06-10 16:13:55 -0400

    Working in chatroom service/controller

--- packages/backend/src/chatroom/chatroom.service.ts
+++ packages/backend/src/chatroom/chatroom.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateChatroomDto } from './dto/create-chatroom.dto';
 import { UpdateChatroomDto } from './dto/update-chatroom.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { Chatroom } from './entities/chatroom.entity';
+import { CreateChatDto } from 'src/chat/dto/create-chat.dto';
 
 @Injectable()
 export class ChatroomService {
+  constructor(private prisma: PrismaService) { }
   async create(createChatroomDto: CreateChatroomDto) {
-    const chatroom = this.prisma
+    const chatroom = this.prisma.chatroom.create({
+      data: {
+        chatroomOwner: createChatroomDto.chatroomOwner,
+        
+      }
+    });
     return await ;
   }
 
