2978425afe7ba68f646e02ee8040904a5d429585
yann <yan_1731@hotmail.com>
2023-06-11 16:04:59 -0400

    Work on chatroom service

--- packages/backend/src/chatroom/chatroom.controller.ts
+++ packages/backend/src/chatroom/chatroom.controller.ts
@@ -19,16 +19,16 @@ export class ChatroomController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.chatroomService.findOne(+id);
+    return this.chatroomService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateChatroomDto: UpdateChatroomDto) {
-    return this.chatroomService.update(+id, updateChatroomDto);
+    return this.chatroomService.update(id, updateChatroomDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.chatroomService.remove(+id);
+    return this.chatroomService.remove(id);
   }
 }
