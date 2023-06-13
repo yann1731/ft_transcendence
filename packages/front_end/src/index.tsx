0e65596ade4ae4608b2f8d06a1ca12955ac44bb3
Marc-Andre Lord <mandrelord@gmail.com>
2023-06-13 15:09:53 -0400

    Theme implementation in progress

--- packages/front_end/src/index.tsx
+++ packages/front_end/src/index.tsx
@@ -1,20 +1,23 @@
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/system';
 import { theme } from './Theme';
 import { BrowserRouter } from 'react-router-dom';
 import App from './Components/App';
 import './App.css';
+import { store } from "./store/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <ThemeProvider theme={theme}>
+    <Provider store={store}>
       <div>
-        <App></App>
+        <App />
       </div>
-      </ThemeProvider> 
+    </Provider> 
     </BrowserRouter>
 );
 
