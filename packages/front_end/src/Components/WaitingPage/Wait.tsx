86c48ca41f8f5269f6e691c12425ead9e63efbff
yann <yan_1731@hotmail.com>
2023-06-18 16:33:57 -0400

    Work on waiting page after login

---
+++ packages/front_end/src/Components/WaitingPage/Wait.tsx
@@ -0,0 +1,12 @@
+import BackgroundContainer from '../../Background';
+import Wait from './WaitingPage';
+
+export default function LoginHome() {
+    return (
+        <div>
+            <BackgroundContainer>
+                <Wait />
+            </BackgroundContainer>
+        </div>
+    )
+}
---
+++ packages/front_end/src/Components/WaitingPage/WaitingPage.tsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../../Theme';
+
+export default function Wait() {
+    
+}
