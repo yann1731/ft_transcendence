97d73e137be1286056a0da9eb9f4265b35462265
yann <yan_1731@hotmail.com>
2023-06-19 09:06:27 -0400

    Make auth module

--- packages/backend/src/auth/auth.service.ts
+++ packages/backend/src/auth/auth.service.ts
@@ -26,9 +26,12 @@ import { Axios } from "axios";
 @Injectable({})
 export class AuthService {
 
-    oauthCallback() {
+    async oauthCallback(code: string) {
+        console.log('yeah boi authenticatin');
+        console.log(code);
         const uid: string =  'u-s4t2ud-47600cc08a77769cea8bec6cacdd6ef77df4be8fbb4984a8b9435f3cdddee480';
         const secret: string = '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';
+        return await code;
     }
 
 
