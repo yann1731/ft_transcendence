9e1ae0296fcb3e8073426688dfcdf4195f650e77
Marc-Andre Lord <mandrelord@gmail.com>
2023-06-29 13:40:17 -0400

    Real time filtered search for channels implemented

--- packages/front_end/src/Components/ChatPage/FriendBox.tsx
+++ packages/front_end/src/Components/ChatPage/FriendBox.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import MyFriends from './ChatComponents/Friends';
-import Autocomplete from '@mui/material/Autocomplete';
 import { useState, ChangeEvent } from 'react';
 
   export default function FriendBox() {
