beb4882dde7ee1209237294470191bd2ffb47e65
fenix144 <yan_1731@hotmail.com>
2023-07-09 13:03:55 -0400

    Add protection for routes

--- packages/front_end/src/Components/HomePage/Home.tsx
+++ packages/front_end/src/Components/HomePage/Home.tsx
@@ -2,14 +2,29 @@ import ResponsiveAppBar from '../ToolBar';
 import BackgroundContainer from '../../Background';
 import DashboardContainer from './Dashboard';
 import { CssBaseline } from '@mui/material';
-import React from 'react';
+import React, { useContext } from 'react';
 import PongGame from './Game';
 import HomeGameBar from './HomeGameBar';
 import InGameChatContainer from './InGameChat';
 import Box from '@mui/material/Box';
 import InGameChatBar from './InGameChatBar';
+import { UserContext } from 'Contexts/userContext';
+import SignIn from 'Components/Login/LoginPage';
+import LoginToolBar from 'Components/Login/LoginToolBar';
 
 export default function Home() {
+	const { user } = useContext(UserContext);
+
+	if (!user) {
+		return (
+			<div>
+                <BackgroundContainer>
+                    <LoginToolBar />
+                    <SignIn />
+                </BackgroundContainer>
+            </div>
+		)
+	}
 	return (
 		  <React.Fragment>
 				<CssBaseline />
