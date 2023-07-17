478b5cdf4a3998fac8b7f444e0f5b9e423460249
Yann <yan_1731@hotmail.com>
2023-07-17 15:24:10 -0400

    Add missing stuff from the merge. (#68)

--- packages/backend/src/auth/auth.controller.ts
+++ packages/backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { responseEncoding } from 'axios';
 
@@ -9,5 +9,10 @@ export class AuthController {
   @Post('oauth') //calls 42 oauth api with provided code
   oauthCallback(@Body('code') code: string) {
     return this.authService.oauthCallback(code);
+  }
+
+  @Post('refreshToken/:id') //grabs a new token when old one is expired
+  refreshCallback(@Param('id') id: string) {
+    return this.authService.refreshCallback(id);
   }
 }
--- packages/backend/src/chat/chat.module.ts
+++ packages/backend/src/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  providers: [ChatGateway, ChatService],
+  imports: [PrismaModule],
 })
 export class ChatModule {}
