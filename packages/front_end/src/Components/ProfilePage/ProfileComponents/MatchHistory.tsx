ad4845e969b94194961924aeb673aca354d4fed0
Marc-Andre <mandrelord@gmail.com>
2023-07-28 08:55:45 -0400

    Match History visuals done

--- packages/front_end/src/Components/ProfilePage/ProfileComponents/MatchHistory.tsx
+++ packages/front_end/src/Components/ProfilePage/ProfileComponents/MatchHistory.tsx
@@ -1,112 +1,53 @@
 import * as React from 'react';
-import { Box, Popover, Grid, Paper, Typography } from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import { experimentalStyled as styled } from '@mui/material/styles';
+import { Box, Typography, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 
 export default function MatchHistory() {
     
-    const [matchType, setMatch] = React.useState('');
+    const [open, setOpen] = React.useState(false);
     
-    const handleChange = (event: SelectChangeEvent) => {
-        setMatch(event.target.value);
-    }
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const handleHover = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    }
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleClickOpen = () => {
+        setOpen(true);
     }
 
-    const Item = styled(Paper)(({ theme }) => ({
-        ...theme.typography.body2,
-        padding: theme.spacing(1),
-        textAlign: 'center',
-        backgroundColor: theme.palette.mode === 'dark' ? '#2B375E' : '#D4DEFF',
-      }));
-
-    const open = Boolean(anchorEl);
-    const id = open ? 'match-history-popover' : undefined;
+    const handleClickClose = () => {
+        setOpen(false);
+    }
 
     return (
         <Box sx={{ width: '99%', textAlign: 'center', marginTop: '15px' }}>
-            <Item>MATCH HISTORY</Item>
-        <Box sx={{ width: '99%', textAlign: 'center', marginTop: '15px', display: 'flex' }}>
-            <Box sx={{ width: '33%', marginRight: '5px' }}>
-            <Item>
-                <Typography
-                    aria-owns={open ? 'mouse-over-popover' : undefined }
-                    aria-haspopup="true"
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleClose}
-                    >
-                    1 vs 1
-                </Typography>
-            </Item>
-            </Box>
-            <Box sx={{ width: '33%', marginRight: '5px' }}>
-            <Item>
-                <Typography
-                    aria-owns={open ? 'mouse-over-popover' : undefined }
-                    aria-haspopup="true"
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleClose}
-                    >
-                    2 vs 2
-                </Typography>
-            </Item>
-            </Box>
-            <Box sx={{ width: '33%' }}>
-            <Item>
-                <Typography
-                    aria-owns={open ? 'mouse-over-popover' : undefined }
-                    aria-haspopup="true"
-                    onMouseEnter={handleHover}
-                    onMouseLeave={handleClose}
-                    >
-                    1 vs 3
-                </Typography>
-            </Item>
-            </Box>
-            <Popover
-                id="mouse-over-popover"
-                sx={{ pointerEvents: 'none', }}
-                open={open}
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                transformOrigin={{ 
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                onClose={handleClose}
-                disableRestoreFocus
-                >
-                <Box sx={{ p: 2 }}>
-                    <div>
-                        <Grid container rowSpacing={3}>
-                            <Grid item sx={{ width: '99%' }}>
-                                <Item>Match history goes here</Item>
-                            </Grid>
-                            <Grid item sx={{ width: '99%' }}>
-                                <Item>Match history goes here</Item>
-                            </Grid>
-                            <Grid item sx={{ width: '99%' }}>
-                                <Item>Match history goes here</Item>
-                            </Grid>
-                            <Grid item sx={{ width: '99%' }}>
-                                <Item>Match history goes here</Item>
-                            </Grid>
-                        </Grid>
-                    </div>
+            MATCH HISTORY
+            <Box sx={{ width: '99%', textAlign: 'center', marginTop: '15px', display: 'flex' }}>
+                <Box sx={{ width: '33%', marginRight: '5px' }}>
+                    <Button className="profilePageButtons" onClick={handleClickOpen}>
+                        <Typography>
+                            1 vs 1
+                        </Typography>
+                    </Button>
                 </Box>
-            </Popover>
-        </Box>
+                <Box sx={{ width: '33%', marginRight: '5px' }}>
+                    <Button className="profilePageButtons" onClick={handleClickOpen}>
+                        <Typography>
+                            2 vs 2
+                        </Typography>
+                    </Button>
+                </Box>
+                <Box sx={{ width: '33%' }}>
+                    <Button className="profilePageButtons" onClick={handleClickOpen}>
+                        <Typography>
+                            1 vs 3
+                        </Typography>
+                    </Button>
+                </Box>
+            </Box>
+            <Dialog open={open} onClose={handleClickClose}>
+                <DialogTitle>Match History</DialogTitle>
+                <DialogContent>
+                    MATCH HISTORY GOES HERE
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClickClose} className="profilePageButtons">OK</Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 }
