01f5aa16d0e1b4c891db3519190a10bc1f9ab0e1
Yann <yan_1731@hotmail.com>
2023-08-04 10:43:50 -0400

    Tidyexcept (#83)
    * Tidy exceptions
    
    * Tidy exceptions
    
    * tidy exceptions

--- packages/backend/src/userfriendship/userfriendship.service.ts
+++ packages/backend/src/userfriendship/userfriendship.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserfriendshipDto } from './dto/create-userfriendship.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -7,55 +7,77 @@ export class UserfriendshipService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserfriendshipDto: CreateUserfriendshipDto) { //creates a friendship relationship between a pair of users
-    const userfriendship = await this.prisma.userFriendship.create({
-      data: {
-        userA: {
-          connect: {
-            id: createUserfriendshipDto.userAId
-          }
-        },
-        userB: {
-          connect: {
-            id: createUserfriendshipDto.userBId
-          }
-        }
-      }
-    });
-    if (!userfriendship)
-      throw new BadRequestException;
-    else
-      return userfriendship;
+	try {
+		const userfriendship = await this.prisma.userFriendship.create({
+			data: {
+			  userA: {
+				connect: {
+				  id: createUserfriendshipDto.userAId
+				}
+			  },
+			  userB: {
+				connect: {
+				  id: createUserfriendshipDto.userBId
+				}
+			  }
+			}
+		});
+		if (!userfriendship)
+			throw new BadRequestException('Could not create user friendship');
+		return userfriendship;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
   }
 
   async findAll() { //returns all friendship pairs
-    const userfriendship = await this.prisma.userFriendship.findMany();
-    if (!userfriendship)
-      throw new BadRequestException;
-    else
-      return userfriendship;
+	try {
+		return await this.prisma.userFriendship.findMany();	
+	} catch (error) {
+		console.log(error);
+		throw new InternalServerErrorException('Something went wrong getting user friendships');
+	}
   }
 
   async findAllUF(id: string) {
-    const userfriendships = await this.prisma.userFriendship.findMany({
-      where: {
-        OR: [
-          { userAId: id },
-          { userBId: id }
-        ]
-      }
-    });
-    return userfriendships;
+	try {
+		const userfriendships = await this.prisma.userFriendship.findMany({
+			where: {
+			  OR: [
+				{ userAId: id },
+				{ userBId: id }
+			  ]
+			}
+		});
+		return userfriendships;	
+	} catch (error) {
+		console.log(error);
+		throw new InternalServerErrorException('Something went wrong getting user friendships with specified user id');
+	}
   }
 
   async findOne(id: string) { //returns a friendship pair by id
-    const userfriendship = await this.prisma.userFriendship.findUnique({where: { id }});
-    if (!userfriendship)
-      throw new BadRequestException;
-    else
-      return userfriendship;
+	try {
+		const userfriendship = await this.prisma.userFriendship.findUnique({where: { id }});
+		if (!userfriendship)
+			throw new BadRequestException;
+		return userfriendship;	
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
   }
 
   async remove(id: string) { //removes a friendship pair from the database
-    return await this.prisma.userFriendship.delete({where: { id }});
+	try {
+		const deluf = await this.prisma.userFriendship.delete({where: { id }});
+		if (!deluf)
+			throw new BadRequestException('Could not delete specified user');
+		return deluf;	
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
   }
 }
