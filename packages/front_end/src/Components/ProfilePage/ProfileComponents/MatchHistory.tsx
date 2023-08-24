10e876bed7680079b1c74bb254b52fa15bc1ad23
Marc-Andre <mandrelord@gmail.com>
2023-08-24 15:03:34 -0400

    Match History DONE for real (testing to do)

--- packages/front_end/src/Components/ProfilePage/ProfileComponents/MatchHistory.tsx
+++ packages/front_end/src/Components/ProfilePage/ProfileComponents/MatchHistory.tsx
@@ -7,6 +7,7 @@ import { useContext, useEffect } from 'react';
 export default function MatchHistory() {
     
     const [open, setOpen] = React.useState(false);
+    const [openDialog, setOpenDialog] = React.useState<string | null>(null);
     const [MatchesOne, setMatchDataOne] = React.useState<MatchHistoryOne[]>([]);
     const [MatchesTwo, setMatchDataTwo] = React.useState<MatchHistoryTwo[]>([]);
     const [MatchesThree, setMatchDataThree] = React.useState<MatchHistoryThree[]>([]);
@@ -20,11 +21,13 @@ export default function MatchHistory() {
         getMatchDataThree();
     })
     
-    const handleClickOpen = () => {
+    const handleClickOpen = (dialogType: string) => {
+        setOpenDialog(dialogType);
         setOpen(true);
     }
 
     const handleClickClose = () => {
+        setOpenDialog(null);
         setOpen(false);
     }
 
@@ -120,41 +123,71 @@ export default function MatchHistory() {
             MATCH HISTORY
             <Box sx={{ width: '99%', textAlign: 'center', marginTop: '15px', display: 'flex' }}>
                 <Box sx={{ width: '33%', marginRight: '5px' }}>
-                    <Button className="profilePageButtons" onClick={handleClickOpen}>
+                    <Button className="profilePageButtons" onClick={() => handleClickOpen('1vs1')}>
                         <Typography>
                             1 vs 1
                         </Typography>
                     </Button>
                 </Box>
-                <Dialog open={open} onClose={handleClickClose}>
-                <DialogTitle>Match History</DialogTitle>
-                <DialogContent>
-                    {MatchesOne.map((match, index) => (
-                        <div key={match.id}>
-                            <Typography sx={{ color: 'green' }}>Winner: {winnerUsernames[index]}</Typography>
-                            <Typography sx={{ color: 'red' }}>Loser: {loserUsernames[index]}</Typography>
-                            <Typography>Score: {match.winnerScore} - {match.loserScore}</Typography>
-                        </div>
-                    ))}
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClickClose}>Close</Button>
-                </DialogActions>
-            </Dialog>
+                <Dialog open={openDialog === '1vs1'} onClose={handleClickClose}>
+                    <DialogTitle>1 vs. 1 History</DialogTitle>
+                    <DialogContent>
+                        {MatchesOne.map((match, index) => (
+                            <div key={match.id}>
+                                <Typography sx={{ color: 'green' }}>Winner: {winnerUsernames[index]}</Typography>
+                                <Typography sx={{ color: 'red' }}>Loser: {loserUsernames[index]}</Typography>
+                                <Typography>Score: {match.winnerScore} - {match.loserScore}</Typography>
+                            </div>
+                        ))}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClickClose}>Close</Button>
+                    </DialogActions>
+                </Dialog>
                 <Box sx={{ width: '33%', marginRight: '5px' }}>
-                    <Button className="profilePageButtons" onClick={handleClickOpen}>
+                    <Button className="profilePageButtons" onClick={() => handleClickOpen('2vs2')}>
                         <Typography>
                             2 vs 2
                         </Typography>
                     </Button>
                 </Box>
+                <Dialog open={openDialog === '2vs2'} onClose={handleClickClose}>
+                    <DialogTitle>2 vs. 2 History</DialogTitle>
+                    <DialogContent>
+                        {MatchesOne.map((match, index) => (
+                            <div key={match.id}>
+                                <Typography sx={{ color: 'green' }}>Winner: {winnerUsernames[index]}</Typography>
+                                <Typography sx={{ color: 'red' }}>Loser: {loserUsernames[index]}</Typography>
+                                <Typography>Score: {match.winnerScore} - {match.loserScore}</Typography>
+                            </div>
+                        ))}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClickClose}>Close</Button>
+                    </DialogActions>
+                </Dialog>
                 <Box sx={{ width: '33%' }}>
-                    <Button className="profilePageButtons" onClick={handleClickOpen}>
+                    <Button className="profilePageButtons" onClick={() => handleClickOpen('1vs3')}>
                         <Typography>
                             1 vs 3
                         </Typography>
                     </Button>
                 </Box>
+                <Dialog open={openDialog === '1vs3'} onClose={handleClickClose}>
+                    <DialogTitle>1 vs. 3 History</DialogTitle>
+                    <DialogContent>
+                        {MatchesOne.map((match, index) => (
+                            <div key={match.id}>
+                                <Typography sx={{ color: 'green' }}>Winner: {winnerUsernames[index]}</Typography>
+                                <Typography sx={{ color: 'red' }}>Loser: {loserUsernames[index]}</Typography>
+                                <Typography>Score: {match.winnerScore} - {match.loserScore}</Typography>
+                            </div>
+                        ))}
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClickClose}>Close</Button>
+                    </DialogActions>
+                </Dialog>
             </Box>
         </Box>
     );
